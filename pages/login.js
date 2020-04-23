2c8253e77bde37d9b6a84793be600c35d7081594
Erhan Yakut <yakuter@gmail.com>
2020-04-24 00:02:14 +0300

    update login design

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Typography, Alert } from 'antd'
 import { cache } from 'swr'
 import { message } from 'antd'
 import { Form, FormItem, Input, SubmitButton } from 'formik-antd'
@@ -8,6 +9,8 @@ import * as Yup from 'yup'
 import Router from 'next/router'
 
 import fetch from '../libs/fetch'
+
+const { Title, Paragraph } = Typography
 
 const urlRegExp = /^(?:([a-z0-9+.-]+):\/\/)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/;
 
@@ -23,25 +26,29 @@ const FormItemList = [
     name: 'BaseURL',
     required: true,
     placeholder: process.env.BASE_URL,
-    prefix: <GlobalOutlined />
+    prefix: <GlobalOutlined />,
+    type: "text"
   },
   {
     label: 'Username',
     name: 'Username',
     required: true,
     placeholder: 'Username',
-    prefix: <UserOutlined />
+    prefix: <UserOutlined />,
+    type: "text"
   },
   {
     label: 'Password',
     name: 'Password',
     required: true,
     placeholder: 'Password',
-    prefix: <LockOutlined />
+    prefix: <LockOutlined />,
+    type: "password"
   }
 ]
 
 function LoginPage() {
+  const [errorMessage, setErrorMessage] = React.useState("");
   const onSubmit = React.useCallback(async (values, actions) => {
     try {
       localStorage.setItem('BASE_URL', values.BaseURL)
@@ -53,6 +60,7 @@ function LoginPage() {
       cache.clear()
       Router.push('/')
     } catch (e) {
+      setErrorMessage(e.message)
       message.error(e.message)
     } finally {
       actions.setSubmitting(false)
@@ -68,54 +76,134 @@ function LoginPage() {
     []
   )
 
-  return (
-    <div className="container">
-      <Formik
-        initialValues={initialValues}
-        validationSchema={LoginSchema}
-        onSubmit={onSubmit}
-      >
-        {() => (
-          <Form layout="vertical">
-            {FormItemList.map(
-              ({ label, required, name, placeholder, prefix }) => (
-                <FormItem
-                  label={label}
-                  name={name}
-                  required={required}
-                  key={name}
-                >
-                  <Input
-                    name={name}
-                    placeholder={placeholder}
-                    prefix={prefix}
-                  />
-                </FormItem>
-              )
-            )}
-            <div className="cta">
-              <SubmitButton>Login</SubmitButton>
-            </div>
-          </Form>
-        )}
-      </Formik>
+  const FormItemList = [
+    {
+      label: 'Base URL',
+      name: 'BaseURL',
+      required: true,
+      placeholder: process.env.BASE_URL,
+      prefix: <GlobalOutlined />,
+      type: "text"
+    },
+    {
+      label: 'Username',
+      name: 'Username',
+      required: true,
+      placeholder: 'Username',
+      prefix: <UserOutlined />,
+      type: "text"
+    },
+    {
+      label: 'Password',
+      name: 'Password',
+      required: true,
+      placeholder: 'Password',
+      prefix: <LockOutlined />,
+      type: "password",
+    }
+  ]
 
-      <style jsx>{`
-        .container {
-          max-width: 340px;
-          width: 100%;
-          margin-left: auto;
-          height: 100vh;
-          margin-right: auto;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-        }
-        .cta {
-          margin-top: 10px;
-        }
-      `}</style>
+  const FormItems = () => {
+    return FormItemList.map(
+      ({ label, required, name, placeholder, prefix, type }) => (
+        <FormItem label={label} name={name} required={required} key={name}>
+          <Input name={name} placeholder={placeholder} prefix={prefix} type={type}/>
+        </FormItem>
+      )
+    )
+  }
+
+  return (
+    <div className="login-wrapper">
+      <div className="login-card">
+        <div className="image-box">
+          <img src="/images/login-illustration.svg" alt="Login" />
+        </div>
+        <div className="form-box">
+          <Title level={3}>PassWall</Title>
+          <Paragraph>Login to the Dashboard</Paragraph>
+          <Formik
+            className="login-form"
+            initialValues={{
+              Username: '',
+              Password: '',
+              BaseURL: process.env.BASE_URL
+            }}
+            validationSchema={LoginSchema}
+            onSubmit={onSubmit}
+          >
+            {() => (
+              <Form layout="vertical">
+                <FormItems />
+                {errorMessage && <Alert style={{marginBottom: 15}} showIcon message={errorMessage} type="error"/>}
+                <div className="cta">
+                  <SubmitButton className="btn-submit">Login</SubmitButton>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .login-wrapper {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: calc(100vh - 60px);
+          }
+          .login-card {
+            display: -webkit-box;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            flex-direction: row-reverse;
+            max-width: 1000px;
+            background-color: white;
+            box-shadow: 0 0 40px rgba(0, 0, 0, 0.16);
+            overflow: hidden;
+            margin: 0 auto;
+            border-radius: 12px;
+          }
+          .form-box {
+            -webkit-box-flex: 1;
+            flex: 1 0 100%;
+            max-width: 480px;
+            width: 100%;
+            padding: 60px;
+          }
+          .image-box {
+            display: flex;
+            align-items: flex-end;
+            max-width: 800px;
+            min-height: 100%;
+            padding: 30px 30px 30px 0;
+          }
+          .image-box img {
+            display: block;
+            width: 100%;
+          }
+        `}
+      </style>
     </div>
+    // <div className="container">
+
+    //   <style jsx>{`
+    //     .container {
+    //       max-width: 340px;
+    //       width: 100%;
+    //       margin-left: auto;
+    //       height: 100vh;
+    //       margin-right: auto;
+    //       display: flex;
+    //       flex-direction: column;
+    //       justify-content: center;
+    //     }
+    //     .cta {
+    //       margin-top: 10px;
+    //     }
+    //   `}</style>
+    // </div>
   )
 }
 
