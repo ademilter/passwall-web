b8abec64308bd05e61328f1bfc417fdb68c256f7
KerimKaraman <kerimkaraman@KerimKaraman-MacBook-Pro.local>
2020-04-23 22:57:37 +0300

    Localhost is not a valid URL bug fix.

--- pages/login.js
+++ pages/login.js
@@ -9,10 +9,12 @@ import Router from 'next/router'
 
 import fetch from '../libs/fetch'
 
+const urlRegExp = /^(?:([a-z0-9+.-]+):\/\/)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/;
+
 const LoginSchema = Yup.object().shape({
   Username: Yup.string().required('Required'),
   Password: Yup.string().required('Required'),
-  BaseURL: Yup.string().url().required('Required')
+  BaseURL: Yup.string().matches(urlRegExp, 'BaseURL must be a valid URL')
 })
 
 const FormItemList = [
