cde3408f8d7cfe9fe25b332987840be01681f61c
cyberst <salihoglual@itu.edu.tr>
2020-04-29 04:25:38 +0300

    Added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import 'antd/dist/antd.css';
 import '../styles/app.css';
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <div>
+    <Head>
+      <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </div>
+);
 
 export default App;
