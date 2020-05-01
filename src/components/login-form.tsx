eed91e25f233f790798127ed81f806d56f040b69
Aykut Kardaş <aykutkrds@gmail.com>
2020-05-01 04:02:01 +0300

    Remove useless urlRegExp.

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -9,12 +9,10 @@ import PassTitle from './title';
 
 const { Paragraph } = Typography;
 
-const urlRegExp = /^(?:([a-z0-9+.-]+):\/\/)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/; // eslint-disable-line max-len
-
 const LoginSchema = Yup.object().shape({
   Username: Yup.string().required('Required'),
   Password: Yup.string().required('Required'),
-  BaseURL: Yup.string().matches(urlRegExp, 'BaseURL must be a valid URL'),
+  BaseURL: Yup.string().url('BaseURL must be a valid URL').required('Required'),
 });
 
 const FormItemList = [
